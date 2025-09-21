eeded4eded03d4fb9be56a1d153b78d8e26a31cf
nearjoy <dmitry.sopoff@gmail.com>
2025-09-21 19:54:00 +0400

    fix: Add TwaWebAppProvider to initialize SDK context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { TwaWebAppProvider } from '@twa-dev/sdk'; // 1. Импортируем Provider
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* 2. Оборачиваем наше приложение в этот Provider */}
+    <TwaWebAppProvider>
+      <App />
+    </TwaWebAppProvider>
+  </React.StrictMode>
+);
